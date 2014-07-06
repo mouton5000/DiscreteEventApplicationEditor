3e4938a7857cd21cf24ffbd81cd568af15f4fb7e
Dimitri Watel <watel.dimitri@gmail.com>
2014-07-06 23:23:43 +0200

    Precompile grammars
    Execution is then faster

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from gui.EditorWindow import MainWindow
 
 
 def main():
+    print 'Compile grammar (0/3)'
+    from grammar.grammar import BooleanExpressionParser
+    BooleanExpressionParser.pre_compile_grammar()
+    print 'Compile grammar (1/3)'
+    from grammar.consequencesGrammar import ConsequencesParser
+    ConsequencesParser.pre_compile_grammar()
+    print 'Compile grammar (2/3)'
+    from grammar.tokenGrammar import TokenParametersParser
+    TokenParametersParser.pre_compile_grammar()
+    print 'Compile grammar (3/3)'
+
     app = QApp(sys.argv)
     ex = MainWindow()
 
